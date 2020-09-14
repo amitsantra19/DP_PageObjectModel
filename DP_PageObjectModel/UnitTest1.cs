8a709519c5236fbbac68bd7466b1a44ec980ab22
Amit Kumar Santra <mailme.amitsantra@gmail.com>
2020-09-14 11:38:10 +0530

    Test Checkin

--- DP_PageObjectModel/UnitTest1.cs
+++ DP_PageObjectModel/UnitTest1.cs
@@ -7,7 +7,7 @@ namespace DP_PageObjectModel
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void PageObjectModelTests()
         {
         }
     }
